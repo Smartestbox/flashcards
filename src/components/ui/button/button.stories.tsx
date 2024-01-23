7ca9fbbdbfb0e002ab894e079e93c171e201049c
Igor Olkhov <smartestbox@gmail.com>
2024-01-23 22:14:38 +0100

    Add logout icons to storybook buttons

--- src/components/ui/button/button.stories.tsx
+++ src/components/ui/button/button.stories.tsx
@@ -1,5 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { Logout } from '@/assets/icons'
+
 import { Button } from './'
 
 const meta = {
@@ -22,41 +24,47 @@ type Story = StoryObj<typeof meta>
 
 export const PrimaryButton: Story = {
   args: {
-    children: 'Button',
+    children: 'Primary button',
     variant: 'primary',
   },
 }
 
 export const PrimaryButtonWithIcon: Story = {
   args: {
-    children: 'Button',
+    children: 'Primary button logout',
+    disabled: false,
+    icon: <Logout />,
     variant: 'primary',
   },
 }
 
 export const SecondaryButton: Story = {
   args: {
-    children: 'Button',
+    children: 'Secondary button',
+    disabled: false,
     variant: 'secondary',
   },
 }
 export const SecondaryButtonWithIcon = {
   args: {
-    children: 'Button',
+    children: 'Secondary button logout',
+    disabled: false,
+    icon: <Logout />,
     variant: 'secondary',
   },
 }
 
 export const DisabledButton = {
   args: {
-    children: 'Button',
+    children: 'Disabled primary button',
     disabled: true,
   },
 }
 
 export const FullWidthButton: Story = {
   args: {
-    children: 'Full Width Button',
+    children: 'Full width button',
+    disabled: false,
     fullWidth: true,
     variant: 'primary',
   },
