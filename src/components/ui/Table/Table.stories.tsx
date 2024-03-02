3a213fa71d50a41828d62f70f059d08067fcd662
Igor Olkhov <smartestbox@gmail.com>
2024-03-02 12:50:08 +0100

    Table initial commit

---
+++ src/components/ui/Table/Table.stories.tsx
@@ -0,0 +1,14 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Table } from './Table'
+
+const meta = {
+  component: Table,
+  tags: ['autodocs'],
+  title: 'Components/Slider',
+} satisfies Meta<typeof Table>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {}
