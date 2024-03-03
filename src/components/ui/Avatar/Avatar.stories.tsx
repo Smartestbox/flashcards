4353b74f2f49f9927e833c288b703a7a13bf9220
Igor Olkhov <smartestbox@gmail.com>
2024-03-03 22:42:13 +0100

    Make Dropdown and Avatar components

---
+++ src/components/ui/Avatar/Avatar.stories.tsx
@@ -0,0 +1,36 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Avatar } from './Avatar'
+
+const meta = {
+  argTypes: {
+    size: {
+      control: 'radio',
+      description: 'Size for avatar',
+      options: ['small', 'large'],
+    },
+    src: {
+      control: false,
+      description: 'Source image (string)',
+    },
+    title: {
+      description: 'Title for fallback text',
+    },
+  },
+  component: Avatar,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  title: 'components/Avatar',
+} satisfies Meta<typeof Avatar>
+
+export default meta
+
+type Story = StoryObj<typeof meta>
+
+export const Small: Story = {
+  args: {
+    src: 'https://semfamily.ru/wp-content/uploads/2020/07/tree-736885.jpg',
+  },
+}
